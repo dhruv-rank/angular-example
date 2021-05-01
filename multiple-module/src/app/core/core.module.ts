82c3b782920147842c825fe499775fb2fd7e531f
subhashlamba <subhashlammba@gmail.com>
2021-05-01 17:12:49 +0530

    service in core module

--- multiple-module/src/app/core/core.module.ts
+++ multiple-module/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { NotificationService } from '../_services/notification.service';
 
 @NgModule({
   declarations: [],       
@@ -9,5 +10,8 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule, // these are the modules which we are going to use all over the application and it needs to import only once for whole application
                       // so it is better to use in the core module(we can also import them into shared module)
   ],
+  providers:[
+    NotificationService      //it will create same instanc for whole application
+  ]
 })
 export class CoreModule {} // this module must be imported once and only in app module
